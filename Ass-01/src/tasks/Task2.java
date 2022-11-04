553f95cda5d41e9c06578be5d2172f617a7bded8
Ahmad Mujtaba <cadeta6@gmail.com>
2022-11-05 01:42:04 +0500

    feat{Task2}: done with task2

--- Ass-01/src/tasks/Task2.java
+++ Ass-01/src/tasks/Task2.java
@@ -1,4 +1,15 @@
 package tasks;
 
+
 public class Task2 {
+    public Boolean check(String str){
+
+        if(str.startsWith("1")) {
+            return 1 < str.chars().filter(ch -> ch == '1').count();
+        }
+        else if(str.startsWith("0")) {
+            return 1 < str.chars().filter(ch -> ch == '0').count();
+        }
+        return false;
+    }
 }
