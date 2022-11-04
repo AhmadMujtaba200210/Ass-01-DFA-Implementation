4cdd4bb7273e36fab214929b7b51870a536fdd9f
Ahmad Mujtaba <cadeta6@gmail.com>
2022-11-05 01:41:18 +0500

    build{Main.java}: build the main

--- Ass-01/src/build/Main.java
+++ Ass-01/src/build/Main.java
@@ -1,4 +1,17 @@
 package build;
 
+import tasks.Task1;
+import tasks.Task2;
+
 public class Main {
+
+    public static void main(String[] args) {
+//        Task1 task1=new Task1();
+//        System.out.println(task1.check("11001"));//task 1
+
+        Task2 task2=new Task2();
+        System.out.println(task2.check("010111"));//task 2
+
+    }
+
 }
