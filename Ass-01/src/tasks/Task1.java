98f2d9426214ec0d30dab5a6e81721300a5694ca
Ahmad Mujtaba <cadeta6@gmail.com>
2022-11-05 01:41:40 +0500

    feat{Task1.java}: done with task1

--- Ass-01/src/tasks/Task1.java
+++ Ass-01/src/tasks/Task1.java
@@ -1,4 +1,9 @@
 package tasks;
 
+import java.util.regex.Pattern;
+
 public class Task1 {
+    public Boolean check(String str){
+        return Pattern.matches("^(?![^0\\n]*0[^0\\n]*(?:0[^0\\n]*0)*[^0\\n]*$)(?![^1\\n]*1[^1\\n]*(?:1[^1\\n]*1)*[^1\\n]*$)[10]*$",str);
+    }
 }
